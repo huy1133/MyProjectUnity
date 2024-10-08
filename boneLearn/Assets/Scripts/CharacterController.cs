8b3a16d662163cffc950b26866c1fdae244e725b
tranvanhuy <91187622+huy1133@users.noreply.github.com>
2024-10-08 11:06:29 +0700

    update

--- boneLearn/Assets/Scripts/CharacterController.cs
+++ boneLearn/Assets/Scripts/CharacterController.cs
@@ -8,16 +8,30 @@ enum CharacterAreOn
 {
     Ground,
     Air,
-    snout,
-    Rope,
-    water
+    Snout,
+    Water,
+    Rope
 };
 public class CharacterController : MonoBehaviour
 {
     [SerializeField] float offsetX;
+    [SerializeField] float speed;
+    [SerializeField] float forceJump;
+    [SerializeField] Transform pointCenterHead;
+    [SerializeField] Transform pointLastLeg;
+    [SerializeField] LayerMask layerMaskEnvi;
+    [SerializeField] Vector3 offsetSnoutToCharacter;
 
-
-
+    Rigidbody2D rb;
+    Animator animator;
+    CharacterAreOn character;
+    bool canJump;
+    bool canMove;
+    bool isWallLeft;
+    bool isWallRight;
+    bool canCheckForwad;
+    bool isGrap;
+    bool canGrap;
     #region animationVariable
     public Vector2 GrapOffset = new Vector2(0.9f, 4.4f);
     #endregion
@@ -30,24 +44,136 @@ public class CharacterController : MonoBehaviour
     float moveInput;
     float climbInput;
     #endregion
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            canMove = true;
+            rb.velocity = Vector2.zero;
+        }
+    }
     private void Start()
     {
-        moveInput = 0;
+		rb = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+		moveInput = 0;
+        climbInput = 0;
+        character = CharacterAreOn.Ground;
+        canJump = true;
+        canMove = true;
+        isWallLeft = false;
+        isWallRight = false;
+        canCheckForwad = true;
+        isGrap = false;
+        canGrap = false;
     }
     private void Update()
     {
+        Debug.Log(transform.position);
+        //-0.84 offset between character and snout point 
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (character == CharacterAreOn.Ground && canJump && !isGrap)
+            {
+                canJump = false;
+                canMove = false;
+                Invoke("applyForceJump", 0.2f);
+                Invoke("ChangeCanJump", 0.5f);
+            }
+            if (character == CharacterAreOn.Snout&&!isGrap)
+            {
+                isGrap = true;
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if(character == CharacterAreOn.Snout&&!isGrap)
+            {
+                canCheckForwad = false;
+            }
+            if (character == CharacterAreOn.Ground && canGrap)
+            {
+                isGrap = true;
+                character = CharacterAreOn.Snout;
+            }
+        }
+        rayCheckDown();
+        if (canCheckForwad&&character!=CharacterAreOn.Snout)
+        {
+            rayCheckForWard();
+        }
         inputHorizontalDirectly();
+		applyMovemen();
         applyDirection();
-        Debug.Log(Input.GetAxis("Horizontal"));
+        applyAnimation();
     }
-    //void applyMovemen()
-    //{
-    //    if (inputDirectly != 0)
-    //    {
-    //        rb.velocity = new Vector2(inputDirectly * speed, rb.velocity.y);
-    //    }
-    //}
+    void applyForceJump()
+    {
+        Vector3 vectorJump;
+        if (Mathf.Abs(moveInput) > 0.4f)
+        {
+            vectorJump = moveInput > 0 ? new Vector2(1, 1.5f) : new Vector2(-1, 1.5f);
+            vectorJump.Normalize();
+        }
+        else
+        {
+            vectorJump = Vector2.up;
+        }
+        rb.velocity = Vector2.zero;
+        rb.AddForce(vectorJump * forceJump);
+        
+    }
+    void ChangeCanJump()
+    {
+        canJump = true;
+    }
+    void applyAnimation()
+    {
+		animator.SetFloat("Moving", Mathf.Abs(moveInput));
+        if (isGrap)
+        {
+            animator.SetBool("Grap", isGrap);
+        }
+        if (!canJump)
+        {
+            animator.SetBool("IsJump", true);
+        }
+        else
+        {
+            animator.SetBool("IsJump", false);
+        }
+        switch (character)
+        {
+            case CharacterAreOn.Ground:
+                animator.SetBool("GetLand", true);
+                animator.SetBool("IsMove", true);
+                animator.SetBool("IsSwing", false);
+                animator.SetBool("IsFall", false);
+                rb.gravityScale = 2.5f;
+                break;
+            case CharacterAreOn.Air:
+                animator.SetBool("GetLand", false);
+                animator.SetBool("IsMove", false);
+                animator.SetBool("IsFall",true);
+                animator.SetBool("IsSwing", false);
+                rb.gravityScale = 2.5f;
+                canCheckForwad = true;
+                break;
+            case CharacterAreOn.Snout:
+                animator.SetBool("IsSwing", true);
+                animator.SetBool("IsMove", false);
+                animator.SetBool("IsFall", false);
+                break;
+            
+        }
+    }
+    void applyMovemen()
+    {
+        if (moveInput != 0 && canMove)
+        {
+            rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        }
+    }
     void applyDirection()
     {
         float direction = transform.localScale.x;
@@ -61,15 +187,80 @@ public class CharacterController : MonoBehaviour
             transform.position = newPos;    
         }
     }
-
+    #region CheckRay
+    void rayCheckDown()
+    {
+        float rayLength = 4.34f;
+        Vector3 startPoint = pointCenterHead.position;
+        RaycastHit2D hit = Physics2D.Raycast(startPoint, Vector2.down, rayLength, layerMaskEnvi);
+        if (hit.collider != null)
+        {
+            if (hit.collider.CompareTag("Ground"))
+            {
+                character = CharacterAreOn.Ground;
+            }
+            else if (hit.collider.CompareTag("Water"))
+            {
+                character = CharacterAreOn.Water;
+            }
+        }
+        else
+        {
+            character = CharacterAreOn.Air; 
+        }
+        hit = Physics2D.Raycast(startPoint, Vector2.down, rayLength, LayerMask.GetMask("Snout"));
+        if (hit.collider != null)
+        {
+            canGrap = true;
+        }
+        else
+        {
+            canGrap = false;
+        }
+        Debug.DrawRay(startPoint, Vector3.down*rayLength,Color.green);
+    }
+    void rayCheckForWard()
+    {
+        float rayLength = 0.3f;
+        Vector3 startPoint = pointCenterHead.position,
+            VectorFace = (transform.localScale.x>0)?Vector3.right:Vector3.left;
+        RaycastHit2D hit = Physics2D.Raycast(startPoint, VectorFace, rayLength, LayerMask.GetMask("Snout"));
+        if (hit.collider != null)
+        {
+            rb.velocity = Vector3.zero;
+            rb.gravityScale = 0;
+            character = CharacterAreOn.Snout;
+        }
+        hit = Physics2D.Raycast(startPoint, VectorFace, rayLength, LayerMask.GetMask("Enviroment"));
+        isWallLeft = false;
+        isWallRight = false;
+        if (hit.collider != null)
+        {
+            if (hit.collider.CompareTag("Wall"))
+            {
+                if (hit.transform.position.x > transform.position.x)
+                {
+                    isWallRight = true;
+                    isWallLeft = false;
+                }
+                else
+                {
+                    isWallLeft = true;
+                    isWallRight = false;
+                }
+            }
+        }
+        Debug.DrawRay(startPoint, VectorFace * rayLength, Color.green);
+    }
+    #endregion
     #region inputFunction
     void inputHorizontalDirectly()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && character==CharacterAreOn.Ground&&!isWallLeft)
         {
             moveInput = moveInput > 0 ? 0 : moveInput -= sensitivity * Time.deltaTime;
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D) && character == CharacterAreOn.Ground&&!isWallRight)
         {
             moveInput = moveInput < 0 ? 0 : moveInput += sensitivity * Time.deltaTime;
         }
@@ -94,11 +285,11 @@ public class CharacterController : MonoBehaviour
     }
     void inputVerticalDirectly()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) && character == CharacterAreOn.Rope)
         {
             climbInput -= sensitivity * Time.deltaTime;
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S) && character == CharacterAreOn.Rope)
         {
             climbInput += sensitivity * Time.deltaTime;
         }
@@ -128,14 +319,18 @@ public class CharacterController : MonoBehaviour
         Vector2 currentPosition = transform.position;
         Vector2 targetPosition = currentPosition - GrapOffset;
         transform.position = targetPosition;
-        Debug.Log("down");
+        isGrap = false;
+        animator.SetBool("Grap", isGrap);
     }
     public void UpdatePositionAfterGrapingUp()
     {
+        Vector2 faceGrap = GrapOffset;
+        faceGrap.x *= transform.localScale.x;
         Vector2 currentPosition = transform.position;
-        Vector2 targetPosition = currentPosition + GrapOffset;
+        Vector2 targetPosition = currentPosition + faceGrap;
         transform.position = targetPosition;
-        Debug.Log("up");
+        isGrap = false;
+        animator.SetBool("Grap", isGrap);
     }
     #endregion
 }
